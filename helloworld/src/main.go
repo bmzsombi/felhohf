434d361ccd121b7b9f18f4ac70df5e142e91aaf3
bmzsombi <bmzsombi@sch.bme.hu>
2025-04-17 20:59:36 +0200

    upload test

--- helloworld/src/main.go
+++ helloworld/src/main.go
@@ -1,10 +1,45 @@
 package main
 
-import "net/http"
+import (
+	"io"
+	"net/http"
+	"os"
+)
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "static/index.html")
 	})
+	http.HandleFunc("/", uploadFile)
 	http.ListenAndServe(":8443", nil)
+}
+
+func uploadFile(w http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodPost {
+		file, _, err := r.FormFile("file")
+		if err != nil {
+			http.Error(w, "Unable to get file", http.StatusBadRequest)
+			return
+		}
+		defer file.Close()
+
+		// Create a new file in the PVC mount point
+		out, err := os.Create("/mnt/data/uploaded_file") // A PVC mount point
+		if err != nil {
+			http.Error(w, "Unable to create file", http.StatusInternalServerError)
+			return
+		}
+		defer out.Close()
+
+		// Copy the uploaded file to the new file
+		_, err = io.Copy(out, file)
+		if err != nil {
+			http.Error(w, "Unable to save file", http.StatusInternalServerError)
+			return
+		}
+
+		w.Write([]byte("File uploaded successfully!"))
+	} else {
+		http.ServeFile(w, r, "static/index.html")
+	}
 }
